4c0a086419935d178cbd1295f72a2e97543af060
Isaac Buitrago <mightyking7@gmail.com>
2020-07-21 22:33:49 -0500

    Created function to compute the iou of two bounding boxes

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,49 @@ import torch
 import math
 import numpy as np
 
+def load_classes(path):
+    # load class labels at path
+    fp = open(path, "r")
+    names = fp.read().split("\n")[:-1]
+    return names
+
+def bbox_iou(box1, box2, x1y1x2y2=True):
+    """
+    Reeturns the IoU of two bounding boxes
+    :param box1:
+    :param box2:
+    :param x1y1x2y2:
+    :return:
+    """
+
+    if not x1y1x2y2:
+        b1_x1, b1_x2 = box1[:, 0] - box1[:, 2] / 2, box1[:, 0] + box1[:, 2] / 2
+        b1_y1, b1_y2 = box1[:, 1] - box1[:, 3] / 2, box1[:, 1] + box1[:, 3] / 2
+        b2_x1, b2_x2 = box2[:, 0] - box2[:, 2] / 2, box2[:, 0] + box2[:, 2] / 2
+        b2_y1, b2_y2 = box2[:, 1] - box2[:, 3] / 2, box2[:, 1] + box2[:, 2] / 2
+    else:
+        # get coordinates of bounding boxes
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1[:, 0], box1[:, 1], box1[:, 2], box1[:, 3]
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
+
+    # get coordinates of intersection rectangle
+    inter_rect_x1 = torch.max(b1_x1, b2_x1)
+    inter_rect_y1 = torch.max(b1_y1, b2_y1)
+    inter_rect_x2 = torch.max(b1_x2, b2_x2)
+    inter_rect_y2 = torch.min(b1_y2, b2_y2)
+
+    # intersection area
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+
+    # union area
+    b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
+
+    iou = inter_area / (b1_area + b2_area - inter_area + 1e-16)
+
+    return iou
+
+
 def build_targets(pred_boxes, pred_conf, pred_cls, target, anchors, n_anchors, n_classes, grid_size, ignore_thres, img_dim):
 
     nB = target.size(0)
